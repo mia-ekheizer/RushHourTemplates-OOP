7e9df060b47d2298c06bcf072012d50fbe6228bf
Mia Ekheizer <miaekh12@gmail.com>
2024-04-02 21:52:28 +0300

    completed BoardCell

--- part2/BoardCell.h
+++ part2/BoardCell.h
@@ -1,1 +1,6 @@
-// Mia
+template<CellType cell_type, Direction direction, int len_vehicle>
+struct BoardCell {
+    CellType type = cell_type;
+    Direction direction = directiob;
+    int length = len_vehicle;
+};
