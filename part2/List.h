c7ba6a6f6db091f9bc5de2ca7d2f82b7ff9bd70f
Mia Ekheizer <miaekh12@gmail.com>
2024-04-03 21:31:20 +0300

    Co-authored-by: yuval-shasha <yuval-shasha@users.noreply.github.com>

--- part2/List.h
+++ part2/List.h
@@ -6,8 +6,11 @@ struct List {
     int size = (sizeof...(Node)) + 1;
 };
 
+template<typename, typename>
+struct PrependList {};
+
 template<typename Type, typename... TypeList>
-struct PrependList {
+struct PrependList<Type, List<TypeList...>> {
     typedef List<Type, TypeList...> list;
 };
 
@@ -26,4 +29,21 @@ struct GetAtIndex<N, List<TypeHead, Node...>> {
 template<typename TypeHead, typename... Node>
 struct GetAtIndex<0, List<TypeHead, Node...>> {
     typedef TypeHead value;
+};
+
+// declaration of the general case
+template<int N, typename Type, typename... TypeList>
+struct SetAtIndex {};
+
+// recursion for N > 0
+template<int N ,typename Type, typename TypeHead, typename... Node>
+struct SetAtIndex<N, Type, List<TypeHead, Node...>> {
+    typedef typename SetAtIndex<N - 1, Type, List<Node...>>::TypeHead list;
+};
+
+// Specialization for N = 0
+// Changing the value in the Nth node
+template<typename Type, typename TypeHead, typename... Node>
+struct SetAtIndex<0, Type, List<TypeHead, Node...>> {
+    typedef typename PrependList<Type, List<Node...>>::TypeHead list;
 };
